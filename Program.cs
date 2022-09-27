6e1e0088bc58a47f557cc32ec82e80bdbfb403ae
asiyush <110299707+asiyush@users.noreply.github.com>
2022-09-27 21:49:51 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,78 +1,65 @@
-// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
-int num_a; 
-int num_b;
-
-Console.Write("Введите числа: ");
-
-num_a = Convert.ToInt32(Console.ReadLine());
-num_b = Convert.ToInt32(Console.ReadLine());
-
-if (num_a > num_b){
-    Console.WriteLine("Большее число " + num_a); 
-}
-else{
-    Console.WriteLine("Большее число: " + num_b);
-}
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+ 
+// Console.Write("Введи N: ");
+// int N = Convert.ToInt16(Console.ReadLine());
+// Console.Write("Введи M: ");
+// int M = Convert.ToInt16(Console.ReadLine());
+// Console.WriteLine($"N={N}, M={M}");
+// if (M>N)
+// for (int i = N; i <= M; i++)
+//     Console.Write($" {i}");
+// else
+//     for (int i = M; i <= N; i++)
+//         Console.Write($" {i}");
 
 
 
-//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// //Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-int num_a; 
-int num_b;
-int num_c;
-int max;
-
-Console.Write("Введите числа: ");
-
-num_a = Convert.ToInt32(Console.ReadLine());
-num_b = Convert.ToInt32(Console.ReadLine());
-num_c = Convert.ToInt32(Console.ReadLine());
-
-max = num_a;
-
-if (num_b > num_a)
-    max = num_b;
-if (num_c > max)
-    max = num_c;
-    {
-    Console.WriteLine("максимальное число " + max); 
-}
-
-
-
-//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
-int num; 
-
-Console.Write("Введите число: ");
-
-num = Convert.ToInt32(Console.ReadLine());
-
-if (num % 2 == 0)
-    {
-    Console.WriteLine(" Да "); 
-}
-else{
-    Console.WriteLine(" Нет ");
-}
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// SumFromMToN(m, n);
+// void SumFromMToN(int m, int n)
+// {
+//     Console.Write(SumMN(m - 1, n));
+// }
+// int SumMN(int m, int n)
+// {
+//     int res = m;
+//     if (m == n)
+//         return 0;
+//     else
+//     {
+//         m++;
+//         res = m + SumMN(m, n);
+//         return res;
+//     }
+// }
 
 
 
-//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-int num; 
+// Console.WriteLine("Введите два положительных числа: M и N.");
+// int m = InputInt("Введите M: ");
+// int n = InputInt("Введите N: ");
+// Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
 
-Console.Write("Введите число: ");
+// int InputInt(string output)
+// {
+//     Console.Write(output);
+//     return int.Parse(Console.ReadLine());
+// }
 
-num = Convert.ToInt32(Console.ReadLine());
-
-int current = 1;
-
-while (current <= num){
-    if (current % 2 == 0)
-    {
-    Console.WriteLine(current + " ");}
-    current++;
-    }
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//         return n + 1;
+//     if (m > 0 && n == 0)
+//         return Akkerman(m - 1, 1);
+//     else
+//         return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
+ 
